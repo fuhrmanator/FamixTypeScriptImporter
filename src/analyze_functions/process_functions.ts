54eddee5471c90655e27314c574f65340fef11f3
AyechiNour <ayechii.nourr@gmail.com>
2024-05-13 03:32:26 +0200

    join process_functions.ts et helpers_process.ts

--- src/analyze_functions/process_functions.ts
+++ src/analyze_functions/process_functions.ts
@@ -1,9 +1,8 @@
-import { ClassDeclaration, MethodDeclaration, VariableStatement, FunctionDeclaration, VariableDeclaration, InterfaceDeclaration, ParameterDeclaration, ConstructorDeclaration, MethodSignature, SourceFile, ModuleDeclaration, PropertyDeclaration, PropertySignature, Decorator, GetAccessorDeclaration, SetAccessorDeclaration, ExportedDeclarations, CommentRange, EnumDeclaration, EnumMember, TypeParameterDeclaration, TypeAliasDeclaration, SyntaxKind, FunctionExpression, Block, Identifier } from "ts-morph";
+import { ClassDeclaration, MethodDeclaration, VariableStatement, FunctionDeclaration, VariableDeclaration, InterfaceDeclaration, ParameterDeclaration, ConstructorDeclaration, MethodSignature, SourceFile, ModuleDeclaration, PropertyDeclaration, PropertySignature, Decorator, GetAccessorDeclaration, SetAccessorDeclaration, ExportedDeclarations, CommentRange, EnumDeclaration, EnumMember, TypeParameterDeclaration, TypeAliasDeclaration, SyntaxKind, FunctionExpression, Block, Identifier, ExpressionWithTypeArguments, ImportDeclaration } from "ts-morph";
 import * as Famix from "../lib/famix/src/model/famix";
 import { calculate } from "../lib/ts-complex/cyclomatic-service";
 import * as fs from 'fs';
 import { logger , entityDictionary } from "../analyze";
-import * as Helpers from "./helpers_process";
 
 export const methodsAndFunctionsWithId = new Map<number, MethodDeclaration | ConstructorDeclaration | GetAccessorDeclaration | SetAccessorDeclaration | FunctionDeclaration | FunctionExpression>(); // Maps the Famix method, constructor, getter, setter and function ids to their ts-morph method, constructor, getter, setter or function object
 export const accessMap = new Map<number, ParameterDeclaration | VariableDeclaration | PropertyDeclaration | EnumMember>(); // Maps the Famix parameter, variable, property and enum value ids to their ts-morph parameter, variable, property or enum member object
@@ -12,6 +11,66 @@ export const interfaces = new Array<InterfaceDeclaration>(); // Array of all the
 export const modules = new Array<SourceFile>(); // Array of all the source files which are modules
 export const exportedMap = new Array<ReadonlyMap<string, ExportedDeclarations[]>>(); // Array of all the exports
 export let currentCC: unknown; // Stores the cyclomatic complexity metrics for the current source file
+
+/**
+ * Checks if the file has any imports or exports to be considered a module
+ * @param sourceFile A source file
+ * @returns A boolean indicating if the file is a module
+ */
+function ismodule(sourceFile: SourceFile): boolean {
+    return sourceFile.getImportDeclarations().length > 0 || sourceFile.getExportedDeclarations().size > 0;
+}
+
+/**
+ * Gets the path of a module to be imported
+ * @param i An import declaration
+ * @returns The path of the module to be imported
+ */
+export function getModulePath(i: ImportDeclaration): string {
+    let path: string;
+    if (i.getModuleSpecifierSourceFile() === undefined) {
+        if (i.getModuleSpecifierValue().substring(i.getModuleSpecifierValue().length - 3) === ".ts") {
+            path = i.getModuleSpecifierValue();
+        }
+        else {
+            path = i.getModuleSpecifierValue() + ".ts";
+        }
+    }
+    else {
+        path = i.getModuleSpecifierSourceFile().getFilePath();
+    }
+    return path;
+}
+
+/**
+ * Gets the interfaces implemented or extended by a class or an interface
+ * @param interfaces An array of interfaces
+ * @param subClass A class or an interface
+ * @returns An array of InterfaceDeclaration and ExpressionWithTypeArguments containing the interfaces implemented or extended by the subClass
+ */
+export function getImplementedOrExtendedInterfaces(interfaces: Array<InterfaceDeclaration>, subClass: ClassDeclaration | InterfaceDeclaration): Array<InterfaceDeclaration | ExpressionWithTypeArguments> {
+    let impOrExtInterfaces: Array<ExpressionWithTypeArguments>;
+    if (subClass instanceof ClassDeclaration) {
+        impOrExtInterfaces = subClass.getImplements();
+    }
+    else {
+        impOrExtInterfaces = subClass.getExtends();
+    }
+
+    const interfacesNames = interfaces.map(i => i.getName());
+    const implementedOrExtendedInterfaces = new Array<InterfaceDeclaration | ExpressionWithTypeArguments>();
+
+    impOrExtInterfaces.forEach(i => {
+        if (interfacesNames.includes(i.getExpression().getText())) {
+            implementedOrExtendedInterfaces.push(interfaces[interfacesNames.indexOf(i.getExpression().getText())]);
+        }
+        else {
+            implementedOrExtendedInterfaces.push(i);
+        }
+    });
+
+    return implementedOrExtendedInterfaces;
+}
 
 /**
  * Builds a Famix model for an array of source files
@@ -36,7 +95,7 @@ export function processFiles(sourceFiles: Array<SourceFile>): void {
  * @param f A source file
  */
 function processFile(f: SourceFile): void {
-    const isModule = Helpers.isModule(f);
+    const isModule = ismodule(f);
 
     if (isModule) {
         modules.push(f);
@@ -639,7 +698,7 @@ export function processImportClauses(modules: Array<SourceFile>, exports: Array<
     logger.info(`processImportClauses: Creating import clauses:`);
     modules.forEach(module => {
         module.getImportDeclarations().forEach(impDecl => {
-            const path = Helpers.getModulePath(impDecl);
+            const path = getModulePath(impDecl);
 
             impDecl.getNamedImports().forEach(namedImport => {
                 logger.debug(`processImportClauses: Importing (named) ${namedImport.getName()} from ${impDecl.getModuleSpecifierValue()}`);
@@ -702,7 +761,7 @@ export function processInheritances(classes: ClassDeclaration[], interfaces: Int
         }
 
         logger.debug(`processInheritances: Checking interface inheritance for ${cls.getName()}`);
-        const implementedInterfaces = Helpers.getImplementedOrExtendedInterfaces(interfaces, cls);
+        const implementedInterfaces = getImplementedOrExtendedInterfaces(interfaces, cls);
         implementedInterfaces.forEach(impInter => {
             entityDictionary.createFamixInheritance(cls, impInter);
 
@@ -712,7 +771,7 @@ export function processInheritances(classes: ClassDeclaration[], interfaces: Int
 
     interfaces.forEach(inter => {
         logger.debug(`processInheritances: Checking interface inheritance for ${inter.getName()}`);
-        const extendedInterfaces = Helpers.getImplementedOrExtendedInterfaces(interfaces, inter);
+        const extendedInterfaces = getImplementedOrExtendedInterfaces(interfaces, inter);
         extendedInterfaces.forEach(extInter => {
             entityDictionary.createFamixInheritance(inter, extInter);
 
