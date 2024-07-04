872e39d7e0884efb4c112c336323e53025296f00
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2024-07-04 11:27:19 -0400

    test avec nouveau cas de concrétisation de classe

--- test/concreteAndGenericParameter.test.ts
+++ test/concreteAndGenericParameter.test.ts
@@ -16,18 +16,29 @@ project.createSourceFile("./src/parametrizableClass.ts",
 
 class ClassB extends ClassA<string>
 
+class ClassC<U> extends ClassA<U> {
+}
+
 `);
 
 const fmxRep = importer.famixRepFromProject(project);
 
 describe('Tests for concrete and generic parameter', () => {
     
-    it("should contain a concrete and generic parameter 'V'", () => {
+    it("should contain a concrete and generic parameter ''", () => {
         const classA = Array.from(fmxRep._getAllEntitiesWithType("ParametricClass") as Set<ParametricClass>).find(v => v.getName() === "ClassA");
         expect(classA).toBeTruthy();
         const param = classA?.getGenericParameters();
         const firstParameter = param?.values().next().value;
         expect(firstParameter?.getName()).toBe('V');
+    });
+
+    it("should contain a generic parameter 'U'", () => {
+        const classC = Array.from(fmxRep._getAllEntitiesWithType("ParametricClass") as Set<ParametricClass>).find(v => v.getName() === "ClassC");
+        expect(classC).toBeTruthy();
+        const param = classC?.getGenericParameters();
+        const firstParameter = param?.values().next().value;
+        expect(firstParameter?.getName()).toBe('U');
     });
 
 });
