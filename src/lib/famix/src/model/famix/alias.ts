f8f1a60a32cc7e91fa89c330f2f9e37048435b2c
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2024-07-30 00:12:51 -0400

    fix Unknow properties, look ahead for the Alias type

--- src/lib/famix/src/model/famix/alias.ts
+++ src/lib/famix/src/model/famix/alias.ts
@@ -35,7 +35,7 @@ export class Alias extends NamedEntity {
 
   public addPropertiesToExporter(exporter: FamixJSONExporter): void {
     super.addPropertiesToExporter(exporter);
-    exporter.addProperty("parentBehaviouralEntity", this.getParentEntity());
+    exporter.addProperty("parentType", this.getParentEntity());
     exporter.addProperty("aliasedEntity", this.getAliasedEntity());
   }
 }
