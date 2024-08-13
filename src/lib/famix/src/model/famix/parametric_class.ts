096aab7b7cf6d16d62dc4aada21f27722a1ba6ac
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2024-08-13 12:52:12 -0400

    parametric_class

--- src/lib/famix/src/model/famix/parametric_class.ts
+++ src/lib/famix/src/model/famix/parametric_class.ts
@@ -6,38 +6,38 @@ import { PrimitiveType } from "./primitive_type";
 
 export class ParametricClass extends Class {
 
-  private _genericParameters: Set<ParameterType> = new Set();
+    private _genericParameters: Set<ParameterType> = new Set();
 
-  public addGenericParameter(genericParameter: ParameterType): void {
-    if (!this._genericParameters.has(genericParameter)) {
-      this._genericParameters.add(genericParameter);
-      genericParameter.parentGeneric = this;
+    public addGenericParameter(genericParameter: ParameterType): void {
+        if (!this._genericParameters.has(genericParameter)) {
+            this._genericParameters.add(genericParameter);
+            genericParameter.parentGeneric = this;
+        }
     }
-  }
 
-  clearGenericParameters(): void {
-    this._genericParameters.clear();
-  }
-
-  private _concreteParameters: Set<PrimitiveType | Class | Interface> = new Set();
-
-  public addConcreteParameter(concreteParameter: PrimitiveType | Class | Interface ): void {
-    if (!this._concreteParameters.has(concreteParameter)) {
-      this._concreteParameters.add(concreteParameter);
+    clearGenericParameters(): void {
+        this._genericParameters.clear();
     }
-  }
 
-  public getJSON(): string {
-    const json: FamixJSONExporter = new FamixJSONExporter("ParametricClass", this);
-    this.addPropertiesToExporter(json);
-    return json.getJSON();
-  }
+    private _concreteParameters: Set<PrimitiveType | Class | Interface> = new Set();
 
-  public addPropertiesToExporter(exporter: FamixJSONExporter): void {
-    super.addPropertiesToExporter(exporter);
-    exporter.addProperty("genericParameters", this.genericParameters);
-    exporter.addProperty("concreteParameters", this.concreteParameters);
-  }
+    public addConcreteParameter(concreteParameter: PrimitiveType | Class | Interface): void {
+        if (!this._concreteParameters.has(concreteParameter)) {
+            this._concreteParameters.add(concreteParameter);
+        }
+    }
+
+    public getJSON(): string {
+        const json: FamixJSONExporter = new FamixJSONExporter("ParametricClass", this);
+        this.addPropertiesToExporter(json);
+        return json.getJSON();
+    }
+
+    public addPropertiesToExporter(exporter: FamixJSONExporter): void {
+        super.addPropertiesToExporter(exporter);
+        exporter.addProperty("genericParameters", this.genericParameters);
+        exporter.addProperty("concreteParameters", this.concreteParameters);
+    }
 
     get genericParameters(): Set<ParameterType> {
         return this._genericParameters;
