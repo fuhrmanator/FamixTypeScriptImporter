91ae66940931b46fc9afe3407d105f732c5eab29
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2024-08-13 13:10:06 -0400

    scoping_entity

--- src/lib/famix/src/model/famix/scoping_entity.ts
+++ src/lib/famix/src/model/famix/scoping_entity.ts
@@ -1,36 +1,35 @@
 import { FamixJSONExporter } from "../../famix_JSON_exporter";
 import { ContainerEntity } from "./container_entity";
-// import { Namespace } from "./namespace";
 import { logger } from "../../../../../analyze";
 import { Module } from "./module";
 
 export class ScopingEntity extends ContainerEntity {
 
-  private childModules: Set<Module> = new Set();
+    private _modules: Set<Module> = new Set();
 
-  public getModules(): Set<Module> {
-    return this.childModules;
-  }
-
-  public addModule(childModule: Module): void {
-    if (!this.childModules.has(childModule)) {
-      logger.debug("Adding module " + childModule.name + " to " + this.name);
-      this.childModules.add(childModule);
-      childModule.parentScope = this;
-    } else {
-      logger.debug("Module " + childModule.name + " already added to " + this.name);
+    public addModule(childModule: Module): void {
+        if (!this._modules.has(childModule)) {
+            logger.debug("Adding module " + childModule.name + " to " + this.name);
+            this._modules.add(childModule);
+            childModule.parentScope = this;
+        } else {
+            logger.debug("Module " + childModule.name + " already added to " + this.name);
+        }
     }
-  }
 
-  
-  public getJSON(): string {
-    const json: FamixJSONExporter = new FamixJSONExporter("ScopingEntity", this);
-    this.addPropertiesToExporter(json);
-    return json.getJSON();
-  }
 
-  public addPropertiesToExporter(exporter: FamixJSONExporter): void {
-    super.addPropertiesToExporter(exporter);
-//    exporter.addProperty("namespaces", this.getModules());
-  }
+    public getJSON(): string {
+        const json: FamixJSONExporter = new FamixJSONExporter("ScopingEntity", this);
+        this.addPropertiesToExporter(json);
+        return json.getJSON();
+    }
+
+    public addPropertiesToExporter(exporter: FamixJSONExporter): void {
+        super.addPropertiesToExporter(exporter);
+        //    exporter.addProperty("namespaces", this.getModules());
+    }
+
+    get modules() {
+        return this._modules;
+    }
 }
