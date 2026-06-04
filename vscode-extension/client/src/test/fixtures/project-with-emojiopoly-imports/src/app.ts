23d25f859960d8976523edea353a2b9c9823ca90
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2026-06-04 10:05:02 -0400

    prevent compilation errors due to missing express dependency in test fixture project

--- vscode-extension/client/src/test/fixtures/project-with-emojiopoly-imports/src/app.ts
+++ vscode-extension/client/src/test/fixtures/project-with-emojiopoly-imports/src/app.ts
@@ -1,15 +1,13 @@
-import express from 'express';
 import _ from 'lodash';
 
 export class App {
-    private app: express.Application;
 
     constructor() {
-        this.app = express();
+        // this.app = express();
     }
 
     start(): void {
         _.noop();
-        this.app.listen(3000);
+        // this.app.listen(3000);
     }
 }
