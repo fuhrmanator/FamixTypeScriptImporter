c62d7064ecd5d5f3217fbd905b200e6fd0fd1b66
Léo Mauré <l.maure1709@gmail.com>
2026-06-11 16:02:21 -0400

    feat: expose API for VSCode extension - add SourceFileChangeType, getTsMorphProject, helpers

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 export { Importer, logger, config, SourceFileChangeType} from "./analyze";
+//export { Importer, config, SourceFileChangeType} from "./analyze";
 export { FamixRepository } from "./lib/famix/famix_repository";
 export { FamixBaseElement } from "./lib/famix/famix_base_element";
 export * from './helpers';
 
 import { Project } from 'ts-morph';
-
+import { logger } from "./analyze";
+logger.info('Initializing ts-morph project');
 export const getTsMorphProject = (tsConfigFilePath: string, baseUrl: string) => {
     return new Project({
         tsConfigFilePath,
         compilerOptions: {
             baseUrl: baseUrl,
         }
-    });
+    });// le main doit etre le point de départ
 };
